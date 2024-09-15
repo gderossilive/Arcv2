493f28fb5ed57c53e000ddc130791bfa504722ba
Gianluca De Rossi <gderossi@microsoft.com>
2024-09-15 18:31:05 +0200

    chore: Improve ArcForServer.ps1 script installation and Azure connection

--- Files/ArcForServer.ps1
+++ Files/ArcForServer.ps1
@@ -29,7 +29,7 @@ $env:CORRELATION_ID = "e0abc3e6-4247-4774-abc7-a6c7fc02de59";
 $env:CLOUD = "AzureCloud";
 
 # Download the package
-Write-Verbose -Message "Downloading the Arc Agent package" -Verbose -foregroundcolor "yellow"
+Write-Verbose -Message "Downloading the Arc Agent package" -Verbose 
 if ($Proxy) {
     Invoke-WebRequest -proxy $Proxy -Uri https://aka.ms/AzureConnectedMachineAgent -OutFile AzureConnectedMachineAgent.msi
 } else {
@@ -40,14 +40,14 @@ if ($Proxy) {
 }
 
 # Install the package
-Write-Verbose -Message "Installing Arc agent" -Verbose -foregroundcolor "yellow"
+Write-Verbose -Message "Installing Arc agent" -Verbose 
 $exitCode=(Start-Process -FilePath msiexec.exe -ArgumentList @("/i", "AzureConnectedMachineAgent.msi" , "/l*v", "installationlog.txt", "/qn") -Wait -Passthru).ExitCode
 if ($exitCode -ne 0) {
     $message = (net helpmsg $exitCode)        
     throw "Installation failed: $message See installationlog.txt for additional details."
 }
 
-Write-Verbose -Message "Connecting Arc agent to Azure" -Verbose -foregroundcolor "yellow"
+Write-Verbose -Message "Connecting Arc agent to Azure" -Verbose 
 if ($AAPLS) {
     $env:PRIVATELINKSCOPE = $AAPLS
     & "$env:ProgramW6432\AzureConnectedMachineAgent\azcmagent.exe" config set proxy.url $Proxy
@@ -76,7 +76,7 @@ if ($AAPLS) {
 
 # Enabling Arc Agent autoupdate
 if ($EnableArcAutoupdate=1) {
-    Write-Verbose -Message "Enabling Arc Agent autoupdate" -Verbose -foregroundcolor "yellow"
+    Write-Verbose -Message "Enabling Arc Agent autoupdate" -Verbose 
     $ServiceManager = (New-Object -com "Microsoft.Update.ServiceManager")
     $ServiceID = "7971f918-a847-4430-9279-4a52d1efe18d"
     $ServiceManager.AddService2($ServiceId,7,"")
@@ -84,7 +84,7 @@ if ($EnableArcAutoupdate=1) {
 
 if ($EnableSSH=1) {
     # Enabling SSH connectivity via Arc
-    Write-Verbose -Message "Enabling SSH connectivity via Arc" -Verbose -foregroundcolor "yellow"
+    Write-Verbose -Message "Enabling SSH connectivity via Arc" -Verbose 
     ## Install the OpenSSH Client
     Add-WindowsCapability -Online -Name OpenSSH.Client~~~~0.0.1.0
     ## Install the OpenSSH Server
